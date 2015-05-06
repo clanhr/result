c80404cd3610cdf108002a494db6aa453caf5ec0
Marcos <marcoslamuria@gmail.com>
2015-05-06 18:17:09 +0100

    rm 1.5

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}}
   :aliases {"all" ["with-profile" "dev:1.6:1.7" "test"]})
