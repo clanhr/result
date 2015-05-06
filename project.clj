57c1604fb0667c27976fac352f67c4697638caf8
Marcos <marcoslamuria@gmail.com>
2015-05-06 17:44:02 +0100

    change project name

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject result "0.1.0-SNAPSHOT"
+(defproject clanhr/result "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/clanhr/result/"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
