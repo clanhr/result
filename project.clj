a0de8a5b22ddbdd25f59c8b5cb361e3e7c29b138
Marcos <marcoslamuria@gmail.com>
2015-05-07 10:25:02 +0100

    add beta2

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/clanhr/result/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}}
   :aliases {"all" ["with-profile" "dev:1.7" "test"]})
