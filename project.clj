a70d58c70d41669612bcd499b1bbf41876943d63
Marcos <marcoslamuria@gmail.com>
2015-05-07 10:49:13 +0100

    fix clojars version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/result "0.1.0-SNAPSHOT"
+(defproject clanhr/result "0.2.0"
   :description "FIXME: write description"
   :url "https://github.com/clanhr/result/"
   :license {:name "Eclipse Public License"
