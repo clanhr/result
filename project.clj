1aa988ff6451ded9b9c523b3a58e4110943bef6e
Marcos <marcoslamuria@gmail.com>
2015-05-07 10:23:19 +0100

    wip

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}}
-  :aliases {"all" ["with-profile" "dev:1.6:1.7" "test"]})
+  :aliases {"all" ["with-profile" "dev:1.7" "test"]})
