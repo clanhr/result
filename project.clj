eebf82992a019acc86597bec91060c6405f0a998
Pedro Santos <donbonifacio@gmail.com>
2015-07-07 11:25:32 +0100

    Clojure 1.7.0

--- project.clj
+++ project.clj
@@ -3,6 +3,4 @@
   :url "https://github.com/clanhr/result"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]]
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta3"]]}}
-  :aliases {"all" ["with-profile" "dev:1.7" "test"]})
+  :dependencies [[org.clojure/clojure "1.7.0"]])
