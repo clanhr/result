acfb7df9c40f317bafc5e42194bdefe5aa6149be
claudiaalves <claudia.alves@rupeal.com>
2016-10-24 15:48:22 +0100

    Add autotest command argument to lein

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :aliases {"autotest" ["with-profile" "+test" "test-refresh"]}
+  :profiles {:test {:env {:secret "test_secret"}
+                    :plugins [[com.jakemccrary/lein-test-refresh "0.17.0"]]}})
